54d1b19c9fb2ae71a8c4e665145283800e05cd14
MihaelaFlorea2000 <mihaelaflorea201@yahoo.com>
2023-04-05 18:02:00 +0300

    Updated .env file

--- public/env.js
+++ public/env.js
@@ -9,7 +9,7 @@ window.env = {
   "TMPDIR": "/var/folders/_j/bxtkm_3965b8w97m4mjd8qgr0000gn/T/",
   "npm_config_global_prefix": "/usr/local",
   "COLOR": "1",
-  "TERM_SESSION_ID": "1cebd731-a8a8-4129-9812-a93623e41bb2",
+  "TERM_SESSION_ID": "e7b2450d-cfb6-4f97-97d6-8e76fe29cb5b",
   "npm_config_noproxy": "",
   "npm_config_registry": "http://registry.npmjs.org/",
   "npm_config_local_prefix": "/Users/cortana/UniDrive/OneDrive - University of Southampton/Y4-Master-SEM2/Web Dev/code/FindAServiceFrontEnd",
