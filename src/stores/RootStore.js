4e134a87569161a6f901c586967a5db42294a425
LeonidGoldberg <leonid.goldberg@outlook.com>
2023-04-13 15:44:46 +0300

    add service store

--- src/stores/RootStore.js
+++ src/stores/RootStore.js
@@ -1,15 +1,17 @@
 import { createContext, useContext } from "react";
 import UserStore from "./UserStore";
+import ServiceStore from "./ServiceStore";
 
 /**
  * Initializing all the stores used for managing app state
  */
 export const store = {
-    userStore: new UserStore()
-}
+  userStore: new UserStore(),
+  serviceStore: new ServiceStore(),
+};
 
 export const StoreContext = createContext(store);
 
 export function useStore() {
-    return useContext(StoreContext)
+  return useContext(StoreContext);
 }
---
+++ src/stores/ServiceStore.js
@@ -0,0 +1,22 @@
+// import axios from "axios";
+import { makeAutoObservable, runInAction } from "mobx";
+import axiosConfig from "../utils/helpers/axiosConfig";
+
+export default class ServiceStore {
+  services = [];
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  getServices(provider = null, category = null, area = null) {
+    axiosConfig()
+      .post("/service/services", arguments)
+      .then((data) => {
+        runInAction(() => {
+          this.services = data.data;
+          console.log("now");
+        });
+      });
+  }
+}
