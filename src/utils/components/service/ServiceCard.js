b25f849d1e58e196c61d99a1eb256be09dbf9822
MihaelaFlorea2000 <mihaelaflorea201@yahoo.com>
2023-04-30 20:34:26 +0100

    Console log to check service image

--- src/utils/components/service/ServiceCard.js
+++ src/utils/components/service/ServiceCard.js
@@ -14,9 +14,9 @@ import {SERVICE_IMAGE} from "../../helpers/constants";
 
 const ServiceCard = (props) => {
 
-  console.log(props.service.serviceImages)
-
   const image = (props.service.serviceImages && props.service.serviceImages.length > 0) ? props.service.serviceImages[0] : SERVICE_IMAGE;
+
+  console.log(image)
 
   return (
     <Card sx={{ borderRadius: 3 }}>
