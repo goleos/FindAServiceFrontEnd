048840026f3179b33fb487bd772997935f23b5cf
NicholasRich <845657927@qq.com>
2023-05-08 19:52:08 +0100

    Create Review UI

--- src/pages/service/ServiceInfo/components/Service.js
+++ src/pages/service/ServiceInfo/components/Service.js
@@ -1,22 +1,30 @@
-import { observer } from "mobx-react";
-import { CircularLoading } from "../../../../utils/components/CircularLoading";
+import {observer} from "mobx-react";
+import {CircularLoading} from "../../../../utils/components/CircularLoading";
 import styled from "@emotion/styled";
-import { border } from "../../../../utils/styles/themeConfig";
+import {border} from "../../../../utils/styles/themeConfig";
 import UnavailableServicePage from "../../UnavailableServicePage";
-import { Alert, Chip, Dialog, DialogContent, Snackbar, useTheme } from "@mui/material";
-import { ProfileImage } from "../../../../utils/components/ProfileImage";
-import { TextIcon } from "../../../../utils/components/TextIcon";
-import { faBellConcierge, faCalendar, faEnvelope, faLocationDot, faPencil } from "@fortawesome/free-solid-svg-icons";
-import { Line, Price } from "../../../../utils/styles/pageStyles";
+import {Alert, Chip, Dialog, DialogContent, Snackbar, useTheme} from "@mui/material";
+import {ProfileImage} from "../../../../utils/components/ProfileImage";
+import {TextIcon} from "../../../../utils/components/TextIcon";
+import {
+    faBellConcierge,
+    faCalendar,
+    faComments,
+    faEnvelope,
+    faLocationDot,
+    faPencil
+} from "@fortawesome/free-solid-svg-icons";
+import {Line, Price} from "../../../../utils/styles/pageStyles";
 import ReadMore from "../../../provider/Profile/components/ReadMore";
-import React, { useState } from "react";
-import { device, SERVICE_IMAGE } from "../../../../utils/helpers/constants";
-import { NavLink, useParams } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, {useState} from "react";
+import {device, SERVICE_IMAGE} from "../../../../utils/helpers/constants";
+import {NavLink, useParams} from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import LoginStore from "../../../../stores/LoginStore";
 import IconButton from "../../../../utils/components/IconButton";
-import { useStore } from "../../../../stores/RootStore";
+import {useStore} from "../../../../stores/RootStore";
 import ServiceRequestForm from "./ServiceRequestForm";
+import ReviewForm from "../../Reviews/ReviewForm";
 import ServiceCategory from "../../../../utils/components/service/ServiceCategory";
 import ServiceRequestList from "../../../../utils/components/service/ServiceRequestList";
 import ManageServiceDialog from "../../../../utils/components/provider/ManageServiceDialog";
@@ -32,7 +40,7 @@ import ReviewStack from "../../Reviews/ReviewStack";
 const Service = (props) => {
     const params = useParams();
 
-    const { userStore, uploadImagesStore } = useStore();
+    const {userStore, uploadImagesStore} = useStore();
 
     const theme = useTheme();
 
@@ -44,24 +52,26 @@ const Service = (props) => {
     const currentUser = userStore.getCurrentUser();
     const customer = !provider && !admin;
 
-    const { serviceRequestsStore } = useStore();
+    const {serviceRequestsStore} = useStore();
 
     const [dialogOpen, setDialogOpen] = useState(false);
+    const [reviewOpen, setReviewOpen] = useState(false);
     const [successAlertOpen, setSuccessAlertOpen] = useState(false);
+    const [successReviewOpen, setSuccessReviewOpen] = useState(false);
 
     // Loading
     if (service === undefined) {
-        return <CircularLoading />;
+        return <CircularLoading/>;
     }
 
     if (service === null) {
-        return <UnavailableServicePage />;
+        return <UnavailableServicePage/>;
     }
 
     const serviceRequests = serviceRequestsStore.getServiceRequests(params.serviceId);
 
     if (serviceRequests === undefined) {
-        return <CircularLoading />;
+        return <CircularLoading/>;
     }
 
     const handleOpenDialog = () => {
@@ -73,110 +83,127 @@ const Service = (props) => {
         setDialogOpen(false);
     };
 
+    const handleOpenReview = () => {
+        setReviewOpen(true);
+    };
+
+    const handleCloseReview = () => {
+        setReviewOpen(false);
+    };
+
     const handleSubmit = async () => {
         setDialogOpen(false);
+
         setSuccessAlertOpen(true);
+    };
+
+    const reviewSubmit =  () => {
+        setReviewOpen(false);
+        setSuccessReviewOpen(true);
     };
 
     const handleCloseSuccessAlert = () => {
         setSuccessAlertOpen(false);
     };
+    const reviewCloseSuccessAlert = () => {
+        setSuccessReviewOpen(false);
+    };
 
     const images = service.serviceImages && service.serviceImages.length > 0 ? service.serviceImages : [SERVICE_IMAGE];
 
-    return (
-        <>
-            <Container>
-                <ImageCarousel images={images} />
-                <Header>
-                    <ServiceDetails>
-                        <ServiceTitle>{service.title}</ServiceTitle>
-                        <DetailsContainer>
-                            <Price>£{service.price}</Price>
-                            <ServiceCategory category={service.category} />
-                        </DetailsContainer>
-                        <AreasCoveredContainer>
-                            <FontAwesomeIcon
+    return (<>
+        <Container>
+            <ImageCarousel images={images}/>
+            <Header>
+                <ServiceDetails>
+                    <ServiceTitle>{service.title}</ServiceTitle>
+                    <DetailsContainer>
+                        <Price>£{service.price}</Price>
+                        <ServiceCategory category={service.category}/>
+                    </DetailsContainer>
+                    <AreasCoveredContainer>
+                        <FontAwesomeIcon
                                 className="fa-fw"
                                 icon={faLocationDot}
                                 color={theme.palette.secondary.main}
-                            />
-                            <TagsContainer>
-                                {service.areasCovered.map((area) => (
-                                    <Chip key={area} tabIndex={-1} label={area} />
-                                ))}
-                            </TagsContainer>
-                        </AreasCoveredContainer>
-                        <AvailabilityContainer>
-                            <FontAwesomeIcon className="fa-fw" icon={faCalendar} color={theme.palette.secondary.main} />
-                            {service.availability.map((day) => (
-                                <Chip key={day} tabIndex={-1} label={day} />
-                            ))}
-                        </AvailabilityContainer>
-                    </ServiceDetails>
-                    <ActionsContainer>
-                        <NavLink to={`/provider/profile/${service.providerId}`}>
-                            <ProviderContainer>
-                                <ProfileImage size="small" image={service.providerProfileImage} />
-                                <NameContainer>
-                                    <Name>
-                                        {service.providerFirstName} {service.providerLastName}
-                                    </Name>
-                                    {/*TODO: Add provider rating*/}
-                                    <TextIcon
+                        />
+                        <TagsContainer>
+                            {service.areasCovered.map((area) => (<Chip key={area} tabIndex={-1} label={area}/>))}
+                        </TagsContainer>
+                    </AreasCoveredContainer>
+                    <AvailabilityContainer>
+                        <FontAwesomeIcon className="fa-fw" icon={faCalendar}
+                                         color={theme.palette.secondary.main}/>
+                        {service.availability.map((day) => (<Chip key={day} tabIndex={-1} label={day}/>))}
+                    </AvailabilityContainer>
+                </ServiceDetails>
+                <ActionsContainer>
+                    <NavLink to={`/provider/profile/${service.providerId}`}>
+                        <ProviderContainer>
+                            <ProfileImage size="small" image={service.providerProfileImage}/>
+                            <NameContainer>
+                                <Name>
+                                    {service.providerFirstName} {service.providerLastName}
+                                </Name>
+                                {/*TODO: Add provider rating*/}
+                                <TextIcon
                                         color={theme.palette.info.main}
                                         text={service.providerEmail}
                                         icon={faEnvelope}
-                                    />
-                                </NameContainer>
-                            </ProviderContainer>
-                        </NavLink>
-                        <ButtonsContainer>
-                            {!customer && currentUser.id === service.providerId && (
-                                <IconButton icon={faPencil} name="Manage" onClick={handleOpenDialog} />
-                            )}
-                            {customer && (
-                                <IconButton icon={faBellConcierge} name="Request" onClick={handleOpenDialog} />
-                            )}
-                        </ButtonsContainer>
-                    </ActionsContainer>
-                </Header>
-                <Line />
-                <Description>
-                    <ReadMore text={service.description} />
-                </Description>
-                {provider ? (
-                    <ManageServiceDialog
-                        providerID={service.providerId}
-                        open={dialogOpen}
-                        onClose={handleCloseDialog}
-                        editingExistingService={true}
-                        editService={service}
-                    />
-                ) : (
-                    <ServiceRequestDialog>
-                        <Snackbar open={successAlertOpen} autoHideDuration={7000} onClose={handleCloseSuccessAlert}>
-                            <Alert severity="success">New service request successfully created</Alert>
-                        </Snackbar>
-                        <Dialog open={dialogOpen} onClose={handleCloseDialog} maxWidth={false} fullWidth>
-                            <DialogContent>
-                                <ServiceRequestForm submit={handleSubmit} />
-                            </DialogContent>
-                        </Dialog>
-                    </ServiceRequestDialog>
-                )}
-            </Container>
-            {serviceRequests.length > 0 && (
-                <>
-                    <Line />
-                    <Subtitle>Requests</Subtitle>
-                    <ServiceRequestList serviceRequests={serviceRequests} byService={true} />
-                </>
-            )}
-            <Subtitle>Reviews</Subtitle>
-            <ReviewStack serviceID={service.id} />
-        </>
-    );
+                                />
+                            </NameContainer>
+                        </ProviderContainer>
+                    </NavLink>
+                    <ButtonsContainer>
+                        {!customer && currentUser.id === service.providerId && (
+                                <IconButton icon={faPencil} name="Manage" onClick={handleOpenDialog}/>)}
+                        {customer && (<IconButton icon={faBellConcierge} name="Request" onClick={handleOpenDialog}/>)}
+                        &emsp;
+                        {customer && (<IconButton color="success" icon={faComments} name="Review" onClick={handleOpenReview}/>)}
+                    </ButtonsContainer>
+                </ActionsContainer>
+            </Header>
+            <Line/>
+            <Description>
+                <ReadMore text={service.description}/>
+            </Description>
+            {provider ? (<ManageServiceDialog
+                    providerID={service.providerId}
+                    open={dialogOpen}
+                    onClose={handleCloseDialog}
+                    editingExistingService={true}
+                    editService={service}
+            />) : (<ServiceRequestDialog>
+                <Snackbar open={successAlertOpen} autoHideDuration={7000}
+                          onClose={handleCloseSuccessAlert}>
+                    <Alert severity="success">New service request successfully created</Alert>
+                </Snackbar>
+                <Dialog open={dialogOpen} onClose={handleCloseDialog} maxWidth={false} fullWidth>
+                    <DialogContent>
+                        <ServiceRequestForm submit={handleSubmit}/>
+                    </DialogContent>
+                </Dialog>
+            </ServiceRequestDialog>)}
+            {(<ReviewDialog>
+                <Snackbar open={successReviewOpen} autoHideDuration={7000}
+                          onClose={reviewCloseSuccessAlert}>
+                    <Alert severity="success">Review successful</Alert>
+                </Snackbar>
+                <Dialog open={reviewOpen} onClose={handleCloseReview} maxWidth={false} fullWidth>
+                    <DialogContent>
+                        <ReviewForm submit={reviewSubmit}/>
+                    </DialogContent>
+                </Dialog>
+            </ReviewDialog>)}
+        </Container>
+        {serviceRequests.length > 0 && (<>
+            <Line/>
+            <Subtitle>Requests</Subtitle>
+            <ServiceRequestList serviceRequests={serviceRequests} byService={true}/>
+        </>)}
+        <Subtitle>Reviews</Subtitle>
+        <ReviewStack serviceID={service.id}/>
+    </>);
 };
 
 export default observer(Service);
@@ -318,3 +345,5 @@ const Description = styled.div`
 const ButtonsContainer = styled.div``;
 
 const ServiceRequestDialog = styled.div``;
+
+const ReviewDialog = styled.div``;
